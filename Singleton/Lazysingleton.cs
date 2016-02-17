6903c0a418f03ecf35cf3a3fb0d752f88709d738
nkprasanna <nkprasasnna@gmail.com>
2016-02-17 13:07:09 +0530

    Singleton added:they were in untracked

---
+++ Singleton/Lazysingleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+   public class LazySingleton
+   {
+       private static readonly Lazy<LazySingleton> _instance = new Lazy<LazySingleton>(() => new LazySingleton());
+       
+       private  LazySingleton()
+       {
+           Console.WriteLine("Lazy Instance created\n");
+       }
+       public static LazySingleton Instance
+       {
+           get
+           {
+               return _instance.Value;
+           }
+       }
+       public void LazyMethod()
+       {
+           Console.WriteLine("Super Lazy\n");
+       }
+   }    
+}
---
+++ Singleton/SingleInstance.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class SingleInstance
+    {
+        private static SingleInstance _instance;
+        private static readonly object _lock = new object();
+
+        private SingleInstance()
+        {
+
+        }
+        public static SingleInstance Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    //Double checking lock ch
+                    lock (_lock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new SingleInstance();
+                            Console.WriteLine("One and only Instance created\n");
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+        public static void MyMethod()
+        {
+            Console.WriteLine("Static Method\n");
+        }
+        public void MyNormalMethod()
+        {
+            Console.WriteLine("Normal Method\n");
+        }
+    }
+
+}
