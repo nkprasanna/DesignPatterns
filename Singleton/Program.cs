432dd6a84ddc0b1e96870677fe488637c72562d6
nkprasanna <nkprasasnna@gmail.com>
2016-02-17 11:01:09 +0530

    Singleton Added

---
+++ Singleton/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingleInstance.Instance.MyNormalMethod();
+
+            SingleInstance.Instance.MyNormalMethod();
+            SingleInstance.MyMethod();
+            LazySingleton.Instance.LazyMethod();
+        }
+    }
+}
